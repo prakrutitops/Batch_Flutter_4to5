c4c00c271bc338263b8b45838400594f43c43d7c
Prakruti_Tops <109515514+prakrutitops@users.noreply.github.com>
2024-05-25 17:08:36 +0530

    commit

---
+++ Currentlocation/currentlocationflutter/lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+import 'homepage.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+// This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      // on below line we are specifying title of our app
+      title: 'GFG',
+      // on below line we are hiding debug banner
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        // on below line we are specifying theme
+        primarySwatch: Colors.green,
+      ),
+      // First screen of our app
+      home: const HomePage(),
+    );
+  }
+}
